49c9c434e7d73f7300c8bd6392b7d63417b5d4be
Samuel <samuelgarabini2.0@gmail.com>
2026-08-05 15:55:39 -0300

    feat: tratamento global de excecoes rfc 7807 e atualizacao do enum multicanal

--- src/main/java/com/raizes/nordeste/RaizesNordesteApplication.java
+++ src/main/java/com/raizes/nordeste/RaizesNordesteApplication.java
@@ -2,8 +2,10 @@ package com.raizes.nordeste;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching; // 1. Adicione este import
 
 @SpringBootApplication
+@EnableCaching // 2. Adicione esta anotação aqui
 public class RaizesNordesteApplication {
 
     public static void main(String[] args) {
